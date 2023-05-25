1a4f2562dfdd8d30fdd869cf269b3681a5f8a0e3
nithinkumarntr <126866701+nithinkumarntr@users.noreply.github.com>
2023-05-25 12:16:34 +0530

    def lcm(a,b):     t=2     res=1     while a>=t and b>=t:         if a%t==0 and b%t==0:             a=a//t             b=b//t             res=res*t         else:             t+=1     return res*a*b a,b = map(int,input().split()) print(lcm(a,b))

--- LCM.py
+++ LCM.py
@@ -1,13 +1,13 @@
-def lcm(a,b):
-    t=2
-    res=1
-    while a>=t and b>=t:
-        if a%t==0 and b%t==0:
-            a=a//t
-            b=b//t
-            res=res*t
+def lcm(a,b): 
+    t=2 
+    res=1 
+    while a>=t and b>=t: 
+        if a%t==0 and b%t==0: 
+            a=a//t 
+            b=b//t 
+            res=res*t 
         else:
-            t+=1
+            t+=1 
     return res*a*b
 a,b = map(int,input().split())
 print(lcm(a,b))
